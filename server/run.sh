b51d4a67b176a0e8c826a126cfb687420afceb80
Metleb1996 <metlebrustemov1996@gmail.com>
2022-07-02 12:06:02 +0400

    run.sh

--- server/run.sh
+++ server/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker run -i -t --rm -p 8080:8080 metleb1996/node-ecommerce-backend
+#docker run -i -t --rm -p 8080:8080 metleb1996/node-ecommerce-backend
+#docker run -d --rm -p 8080:8080 metleb1996/node-ecommerce-backend
+#docker stack deploy -c stack.yml backend
+docker-compose down
+docker-compose -f docker-compose.yml up --build &
