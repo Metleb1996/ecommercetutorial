c411ea353483952f99990253973b37da7bd78712
Metleb1996 <metlebrustemov1996@gmail.com>
2022-07-02 15:41:59 +0400

    MultiProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,21 @@
 import 'package:ecommercetutorial/constants/global_variables.dart';
 import 'package:ecommercetutorial/features/auth/screens/auth_screen.dart';
+import 'package:ecommercetutorial/providers/user_provider.dart';
 import 'package:ecommercetutorial/router.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => UserProvider(),
+        )
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
