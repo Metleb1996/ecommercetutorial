0295398ade11f12d97e1f304d5f310175bc15d8b
Metleb1996 <metlebrustemov1996@gmail.com>
2022-07-02 15:42:53 +0400

    screen switch

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -3,9 +3,13 @@ import 'dart:convert';
 import 'package:ecommercetutorial/constants/error_handling.dart';
 import 'package:ecommercetutorial/constants/global_variables.dart';
 import 'package:ecommercetutorial/constants/utils.dart';
+import 'package:ecommercetutorial/features/home/screens/home_screen.dart';
 import 'package:ecommercetutorial/models/user.dart';
+import 'package:ecommercetutorial/providers/user_provider.dart';
 import 'package:flutter/material.dart';
 import "package:http/http.dart" as http;
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService {
   void signUpUser({
@@ -61,11 +65,19 @@ class AuthService {
           "Content-Type": "application/json; charset=UTF-8",
         },
       );
-      print(res.body);
       httpErrorHandle(
         response: res,
         context: context,
-        onSuccess: () {},
+        onSuccess: () async {
+          SharedPreferences prefs = await SharedPreferences.getInstance();
+          await prefs.setString("x-auth-token", jsonDecode(res.body)['token']);
+          Provider.of<UserProvider>(context, listen: false).setUser(res.body);
+          Navigator.pushNamedAndRemoveUntil(
+            context,
+            HomeScreen.routeName,
+            (route) => false,
+          );
+        },
       );
     } catch (e) {
       showSnackBar(context, e.toString());
