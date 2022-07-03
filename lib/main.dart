4002a3610e79ac2f0d528c6df855caec5e328d6d
Metleb1996 <metlebrustemov1996@gmail.com>
2022-07-03 15:36:57 +0400

    BottomBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'package:ecommercetutorial/common/widgets/bottom_bar.dart';
 import 'package:ecommercetutorial/constants/global_variables.dart';
 import 'package:ecommercetutorial/features/auth/screens/auth_screen.dart';
 import 'package:ecommercetutorial/features/auth/services/auth_service.dart';
-import 'package:ecommercetutorial/features/home/screens/home_screen.dart';
 import 'package:ecommercetutorial/providers/user_provider.dart';
 import 'package:ecommercetutorial/router.dart';
 import 'package:flutter/material.dart';
@@ -54,7 +54,7 @@ class _MyAppState extends State<MyApp> {
       ),
       onGenerateRoute: (settings) => generateRoute(settings),
       home: Provider.of<UserProvider>(context).user.token.isNotEmpty
-          ? const HomeScreen()
+          ? const BottomBar()
           : const AuthScreen(),
     );
   }
