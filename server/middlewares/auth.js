6265e76a3e68fc9a4fe29e9f603a0f361b374ff5
Metleb1996 <metlebrustemov1996@gmail.com>
2022-07-03 14:53:28 +0400

    auth middleware

---
+++ server/middlewares/auth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header("x-auth-token");
+        if(!token) return res.status(401).json({msg: "No auth token, access deniad."});
+        const verified = jwt.verify(token, "verySecretPasswordKey");
+        if (!verified) return res.status(401).json({msg: "Token verification failed, authorization denied."});
+        req.user = verified.id;
+        req.token = token;
+        next();
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+}
+
+module.exports = auth;
