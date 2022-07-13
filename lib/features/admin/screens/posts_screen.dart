f98969ff7477a9cd44ce02480e388b5f0703f211
Metleb1996 <metlebrustemov1996@gmail.com>
2022-07-13 15:45:31 +0400

    navigateToAddProduct

--- lib/features/admin/screens/posts_screen.dart
+++ lib/features/admin/screens/posts_screen.dart
@@ -1,3 +1,4 @@
+import 'package:ecommercetutorial/features/admin/screens/add_product_screen.dart';
 import 'package:flutter/material.dart';
 
 class PostsScreen extends StatefulWidget {
@@ -8,6 +9,10 @@ class PostsScreen extends StatefulWidget {
 }
 
 class _PostsScreenState extends State<PostsScreen> {
+  void navigateToAddProduct() {
+    Navigator.pushNamed(context, AddProductScreen.routeName);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +22,7 @@ class _PostsScreenState extends State<PostsScreen> {
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         tooltip: "Add a Product",
-        onPressed: () {},
+        onPressed: navigateToAddProduct,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
