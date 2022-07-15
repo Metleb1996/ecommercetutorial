a153f7d9c38241835085dd03949cf76067f239a6
Metleb1996 <metlebrustemov1996@gmail.com>
2022-07-15 12:16:58 +0400

    adminRoute

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const express = require('express');
 const mongoose = require("mongoose");
+const adminRouter = require('./routes/admin');
 
 const authRouter = require("./routes/auth");
 
@@ -11,6 +12,7 @@ const DB = "mongodb://root:example@mongo:27017/";
 const app = express();
 app.use(express.json());
 app.use(authRouter);
+app.use(adminRouter);
 
 mongoose.connect(DB, {useNewUrlParser: true})
 .then(()=>{
