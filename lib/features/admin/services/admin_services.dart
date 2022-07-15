415be7d7c41a1849f311ed4255b6d7acb5f1e447
Metleb1996 <metlebrustemov1996@gmail.com>
2022-07-15 12:19:18 +0400

    AdminServices

--- lib/features/admin/services/admin_services.dart
+++ lib/features/admin/services/admin_services.dart
@@ -1,7 +1,14 @@
 import 'dart:io';
 
+import 'package:cloudinary_public/cloudinary_public.dart';
+import 'package:ecommercetutorial/constants/error_handling.dart';
+import 'package:ecommercetutorial/constants/global_variables.dart';
 import 'package:ecommercetutorial/constants/utils.dart';
+import 'package:ecommercetutorial/models/product.dart';
+import 'package:ecommercetutorial/providers/user_provider.dart';
 import 'package:flutter/material.dart';
+import "package:http/http.dart" as http;
+import 'package:provider/provider.dart';
 
 class AdminServices {
   void sellProduct({
@@ -13,7 +20,43 @@ class AdminServices {
     required String category,
     required List<File> images,
   }) async {
-    try {} catch (e) {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    try {
+      final cloudinary = CloudinaryPublic("_cloudName", "_uploadPreset");
+      List<String> imageUrls = [];
+      for (int i = 0; i < images.length; i++) {
+        CloudinaryResponse res =
+            await cloudinary.uploadFile(CloudinaryFile.fromFile(
+          images[i].path,
+          folder: name,
+        ));
+        imageUrls.add(res.secureUrl);
+      }
+      Product product = Product(
+        name: name,
+        description: description,
+        quantity: quantity,
+        images: imageUrls,
+        category: category,
+        price: price,
+      );
+      http.Response res = await http.post(
+        Uri.parse("$uri/admin/add-product"),
+        headers: <String, String>{
+          "Content-Type": "application/json; charset=UTF-8",
+          "x-auth-token": userProvider.user.token,
+        },
+        body: product.toJson(),
+      );
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          showSnackBar(context, "Product Added Successfully!");
+          Navigator.pop(context);
+        },
+      );
+    } catch (e) {
       showSnackBar(context, e.toString());
     }
   }
